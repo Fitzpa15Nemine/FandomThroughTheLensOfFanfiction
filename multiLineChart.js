20111582a700a02bec35bf200e8b0f68df254a0a
Nimanui <fitzpa15@illinois.edu>
2022-07-30 00:46:23 -0400

    Got some basic data to display, need more data cleansing so will refresh the raw

---
+++ multiLineChart.js
@@ -0,0 +1,55 @@
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var svg = d3.select("#bulkChart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+function init(filename){
+    d3.csv(filename, function(data) {
+        var nestStat = d3.nest() 
+            .key(function(d) { return d.fandom;})
+            .entries(data);
+        
+        //console.log(d3.extent(data, function(d) { return d.year; }))
+        var x = d3.scaleLinear()
+            .domain([2000,2022])
+            .range([ 0, width ]);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).ticks(5));
+        
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return +d.fic; })])
+            .range([ height, 0 ]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+        
+        // adding lots of colors
+        var res = nestStat.map(function(d){ return d.key })
+        var color = d3.scaleOrdinal()
+            .domain(res)
+            .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+        
+        // rendering the data
+        svg.selectAll(".line")
+            .data(nestStat)
+            .enter()
+            .append("path")
+                .attr("fill", "none")
+                .attr("stroke", function(d){ return color(d.key) })
+                .attr("stroke-width", 1.5)
+                .attr("d", function(d){
+                return d3.line()
+                    .x(function(d) { return x(d.year - -1 * d.month/12); })
+                    .y(function(d) { return y(+d.fic); })
+                    (d.values)
+                })
+        
+        })
+    }
